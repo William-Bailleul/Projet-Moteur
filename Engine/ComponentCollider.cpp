c995a550f895cda95bc4a20232d9388036aed026
Nadlecy <bruno.cleymand@gmail.com>
2024-02-27 15:59:46 +0100

    component inint

---
+++ Engine/ComponentCollider.cpp
@@ -0,0 +1,9 @@
+#include "ComponentCollider.h"
+
+ComponentCollider::ComponentCollider(GameObject* gameObjectPointer):Component::Component(gameObjectPointer){
+	Init();
+};
+
+void ComponentCollider::Init() {
+
+}

7f0f5f06ca556afa7ea06d5707fe91c65bf75ec5
wdamour <wdamour@gaming.tech>
2024-02-28 10:05:25 +0100

    All transformation

--- Engine/GameObject.cpp
+++ Engine/GameObject.cpp
@@ -1,1 +1,74 @@
 #include "GameObject.h"
+
+
+void GameObject::Transform::Rotate(float yaw, float pitch, float roll) {
+	XMVECTOR  quat;
+	XMVECTOR dir = XMVector3Normalize(XMLoadFloat3(&vDir));
+	XMVECTOR right = XMVector3Normalize(XMLoadFloat3(&vRight));
+	XMVECTOR up = XMVector3Normalize(XMLoadFloat3(&vUp));
+	quat = XMQuaternionRotationAxis(dir, roll);
+	quat *= XMLoadFloat4(&qRot);
+	XMStoreFloat4(&qRot, quat);
+	quat = XMQuaternionRotationAxis(right, pitch);
+	quat *= XMLoadFloat4(&qRot);
+	XMStoreFloat4(&qRot, quat);
+	quat = XMQuaternionRotationAxis(up, roll);
+	quat *= XMLoadFloat4(&qRot);
+	XMStoreFloat4(&qRot, quat);
+
+	XMStoreFloat4x4(&mRot, XMMatrixRotationQuaternion(XMLoadFloat4(&qRot)));
+
+	vRight.x = mRot._11;
+	vRight.y = mRot._12;
+	vRight.z = mRot._13;
+	vUp.x = mRot._21;
+	vUp.y = mRot._22;
+	vUp.z = mRot._23;
+	vDir.x = mRot._31;
+	vDir.y = mRot._32;
+	vDir.z = mRot._33;
+}
+
+void GameObject::Transform::Identity() {
+	mMatrix._11 = 1;
+	mMatrix._12 = 0;
+	mMatrix._13 = 0;
+	mMatrix._21 = 0;
+	mMatrix._22 = 1;
+	mMatrix._23 = 0;
+	mMatrix._31 = 0;
+	mMatrix._32 = 0;
+	mMatrix._33 = 1;
+}
+
+void GameObject::Transform::Scale(float x, float y, float z) {
+	vSca.x = x;
+	vSca.y = y;
+	vSca.z = z;
+	vSca.w = 1;
+	XMVECTOR vScale = XMLoadFloat4(&vSca);
+	XMMATRIX scale = XMMatrixScalingFromVector(vScale);
+	XMStoreFloat4x4(&mSca, scale);
+}
+
+void GameObject::Transform::SetPosition(int x, int y, int z) {
+	vPos.x = x;
+	vPos.y = y;
+	vPos.z = z;
+	mPos._11 = x;
+	mPos._22 = y;
+	mPos._33 = z;
+	mPos._44 = 1;
+}
+
+void GameObject::Transform::TransformObject() {
+	XMMATRIX matrix = XMLoadFloat4x4(&mPos);
+	matrix *= XMLoadFloat4x4(&mSca);
+	matrix *= XMLoadFloat4x4(&mRot);
+	XMStoreFloat4x4(&mMatrix, matrix);
+}
+
+float GetDegreeToRadian(float fAngleDegree)
+{
+	return fAngleDegree * PI / 180.f;
+}
--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -1,4 +1,50 @@
 #pragma once
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+
+#define PI 3.14159265358979323846264338327950288
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
 class GameObject
 {
+	struct Transform
+	{
+		//Scale
+		XMFLOAT4 vSca;
+		XMFLOAT4X4 mSca;
+
+		//Rotate
+		XMFLOAT3 vDir;
+		XMFLOAT3 vRight;
+		XMFLOAT3 vUp;
+		XMFLOAT4 qRot;
+		XMFLOAT4X4 mRot;
+
+		//Position
+		XMFLOAT3 vPos;
+		XMFLOAT4X4 mPos;
+
+		//Matrix of the object
+		XMFLOAT4X4 mMatrix;
+
+		void Identity();
+		void FromMatrix(XMFLOAT3X3* pMat);
+		void UpdateRotationFromVectors();
+		void UpdateRotationFromQuaternion();
+		void UpdateRotationFromMatrix();
+		void UpdateMatrix();
+		void Rotate(float yaw, float pitch, float roll);
+		void RotateYaw(float angle);
+		void RotatePitch(float angle);
+		void RotateRoll(float angle);
+		void RotateWorld(XMFLOAT3X3* pMatrix);
+		void RotateWorldX(float angle);
+		void RotateWorldY(float angle);
+		void RotateWorldZ(float angle);
+		void Scale(float x, float y, float z);
+		void SetPosition(int x, int y, int z);
+		void TransformObject();
+	};
 };

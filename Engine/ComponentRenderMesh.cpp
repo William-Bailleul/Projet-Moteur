efea8a56b64e6d622804354bbb4868813a25962f
Nadlecy <bruno.cleymand@gmail.com>
2024-02-29 15:46:56 +0100

    d

--- Engine/ComponentRenderMesh.cpp
+++ Engine/ComponentRenderMesh.cpp
@@ -1,1 +1,14 @@
 #include "ComponentRenderMesh.h"
+
+ComponentRenderMesh::ComponentRenderMesh(GameObject* gameObjectPointer, Mesh* meshRef, Shader* shaderRef, Texture* textureRef) :Component::Component(gameObjectPointer) {
+	Init(meshRef, shaderRef, textureRef);
+}
+
+void ComponentRenderMesh::Init(Mesh* meshRef, Shader* shaderRef, Texture* textureRef) {
+	refMesh = meshRef;
+	refShader = shaderRef;
+	refTexture = textureRef;
+}
+
+ComponentRenderMesh::~ComponentRenderMesh() {
+}

0c607d06c5f07c9ba9785997423897a668109b1f
Nadlecy <bruno.cleymand@gmail.com>
2024-02-29 15:47:16 +0100

    sdds

--- Engine/ComponentRenderMesh.h
+++ Engine/ComponentRenderMesh.h
@@ -1,5 +1,21 @@
 #pragma once
-class ComponentRenderMesh
+
+#include "Component.h"
+
+struct Mesh;
+class Shader;
+class Texture;
+
+class ComponentRenderMesh : public Component
 {
+	Mesh* refMesh;
+	Shader* refShader;
+	Texture* refTexture;
+
+	ComponentRenderMesh(GameObject* gameObjectPointer, Mesh* meshRef, Shader* shaderRef, Texture* textureRef);
+	~ComponentRenderMesh();
+
+private:
+	void Init(Mesh* meshRef, Shader* shaderRef, Texture* textureRef);
 };
 

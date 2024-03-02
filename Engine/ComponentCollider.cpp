a6f3c4037884c639ef7a0ed7805d5494a53504a9
Nad <bruno.cleymand@gmail.com>
2024-03-02 17:24:06 +0100

    tertre

--- Engine/ComponentCollider.cpp
+++ Engine/ComponentCollider.cpp
@@ -1,9 +1,22 @@
 #include "ComponentCollider.h"
 
-ComponentCollider::ComponentCollider(GameObject* gameObjectPointer):Component::Component(gameObjectPointer){
-	Init();
+ComponentCollider::ComponentCollider(GameObject* gameObjectPointer, BoundaryStyle boundaryStyle) :Component::Component(gameObjectPointer) {
+	Init(boundaryStyle);
 };
 
-void ComponentCollider::Init() {
+void ComponentCollider::Init(BoundaryStyle boundaryStyle) {
+	switch (boundaryStyle) {
+	case RECT:
 
+		break;
+	case BOX:
+
+		break;
+	case SPHERE:
+
+		break;
+
+	default:
+		break;
+	}
 }

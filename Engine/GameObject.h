48cc8c180d523989edd9614f5c1b82a42b8d6c33
wdamour <wdamour@gaming.tech>
2024-03-04 17:18:37 +0100

    componentList & pos

--- Engine/GameObject.h
+++ Engine/GameObject.h
@@ -13,48 +13,16 @@ class Component;
 
 class GameObject
 {
-	struct Transform
-	{
-		//Scale
-		XMFLOAT4 vSca;
-		XMFLOAT4X4 mSca;
-
-		//Rotate
-		XMFLOAT3 vDir;
-		XMFLOAT3 vRight;
-		XMFLOAT3 vUp;
-		XMFLOAT4 qRot;
-		XMFLOAT4X4 mRot;
-
-		//Position
-		XMFLOAT3 vPos;
-		XMFLOAT4X4 mPos;
-
-		//Matrix of the object
-		XMFLOAT4X4 mMatrix;
-
-		void Identity();
-		void FromMatrix(XMFLOAT4X4* pMat);
-		void UpdateRotationFromVectors();
-		void UpdateRotationFromQuaternion();
-		void UpdateRotationFromMatrix();
-		void UpdateMatrix();
-		void Rotate(float yaw, float pitch, float roll);
-		void RotateYaw(float angle);
-		void RotatePitch(float angle);
-		void RotateRoll(float angle);
-		void RotateWorld(XMFLOAT3X3* pMatrix);
-		void RotateWorldX(float angle);
-		void RotateWorldY(float angle);
-		void RotateWorldZ(float angle);
-		void Scale(float x, float y, float z);
-		void SetPosition(int x, int y, int z);
-	};
+private:
+	int m_posX;
+	int m_posY;
+	int m_posZ;
 public:
 	std::vector<Component*> componentList;
 
 	GameObject();
 	~GameObject();
 
+	void Init(int posX, int posY, int posZ);
 	void addComponent(Component* component);
 };

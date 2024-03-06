607c41d0923c2ea4fe4d533282a4542a9c980417
Nadlecy <bruno.cleymand@gmail.com>
2024-03-06 17:18:32 +0100

    script update

--- Engine/ComponentScript.cpp
+++ Engine/ComponentScript.cpp
@@ -1,20 +1,55 @@
 #include "ComponentScript.h"
 
-ComponentScript::ComponentScript(GameObject* gameObjectPointer, string name, string initState) :Component::Component(gameObjectPointer) {
-	Init(name, initState);
+#include "GameTimer.h"
+
+ComponentScript::ComponentScript(GameObject* gameObjectPointer, string name, string initState, GameTimer* timer) :Component::Component(gameObjectPointer) {
+	Init(name, initState, timer);
 };
 
-void ComponentScript::Init(string name, string initState) {
+void ComponentScript::Init(string name, string initState, GameTimer* timer) {
 	scriptName = name;
 	scriptState = initState;
+	gameTimer = timer;
 }
 
-////////////////////////BEHAVIOR FUNCTIONS////////////////////////
+// Main Behavior Function
 void ComponentScript::PlayScript() {
 
 }
 
-////////////////////////QUEUE FUNCTIONS////////////////////////
+
+//////////////////////////////////
+//		OBJECT MANAGEMENT		//
+//////////////////////////////////
+
+//VISIBILITY FUNCTIONS
+//make the object visible
+void ComponentScript::ObjectShow() {
+
+}
+
+//make the object invisible
+void ComponentScript::ObjectHide() {
+
+}
+
+//completely end the object's existence
+void ComponentScript::ObjectDestroy() {
+
+}
+
+// OBJECT SHAPE FUNCTIONS
+
+
+// TIMER FUNCTIONS
+
+
+//////////////////////////////////
+//		SCRIPT MANAGEMENT		//
+//////////////////////////////////
+
+//QUEUE FUNCTIONS
+
 void ComponentScript::AddToQueue(string newInput) {
 	scriptQueue.push_back(newInput);
 }
@@ -27,7 +62,8 @@ void ComponentScript::TrashFront() {
 	scriptQueue.erase(scriptQueue.begin());
 }
 
-////////////////////////STATE FUNCTIONS////////////////////////
+//STATE FUNCTIONS
+
 void ComponentScript::SetState(string newState) {
 	scriptState = newState;
 }
@@ -36,7 +72,8 @@ string ComponentScript::GetState() {
 	return scriptState;
 }
 
-////////////////////////NAME FUNCTIONS////////////////////////
+//NAME FUNCTIONS
+
 string ComponentScript::GetName() {
 	return scriptName;
 }
--- Engine/ComponentScript.h
+++ Engine/ComponentScript.h
@@ -7,30 +7,56 @@
 
 using namespace std;
 
+class GameTimer;
+
 class ComponentScript: public Component
 {
 public:
 
-	ComponentScript(GameObject* gameObjectPointer, string name, string initState);
+	ComponentScript(GameObject* gameObjectPointer, string name, string initState, GameTimer* timer);
 
-	// Behavior Functions
+	// Main Behavior Function
 	void PlayScript();
 
-	// Queue Functions
+	//////////////////////
+	// OBJECT FUNCTIONS	//
+	//////////////////////
+	
+	// Visibility Funcs
+	void ObjectShow();
+	void ObjectHide();
+	void ObjectDestroy();
+
+	// Object Shape Funcs (using the Transform struct)
+	// /?\ void move(normal direction matrix, distance/magnitude)
+	// /?\ void rotate
+	//
+
+	// Timer Funcs (using the GameTimer.h file)
+	
+
+	//////////////////////////
+	//	SCRIPT MANAGEMENT	//
+	//////////////////////////
+	
+	// Queue Funcs
 	void AddToQueue(string newInput); 
 	string ReadFront();
 	void TrashFront();
 
-	// State Functions
+	// State Funcs
 	void SetState(string newState);
 	string GetState();
 
-	// Name Functions
+	// Name Funcs
 	string GetName();
 
 private:
 	string scriptName;
 	string scriptState;
+
+	GameTimer* gameTimer;
+
 	vector<string> scriptQueue;
-	void Init(string name, string initState);
+	void Init(string name, string initState, GameTimer* timer);
 };

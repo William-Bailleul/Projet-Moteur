ab07b7f87cc8cd42e1bb1aa1ccc926670d106dec
Nadlecy <bruno.cleymand@gmail.com>
2024-03-12 12:45:20 +0100

    s

--- Game/GameManager.h
+++ Game/GameManager.h
@@ -1,11 +1,9 @@
 #pragma once
 #include "GameObjectEnemy.h"
 #include "GameObjectShot.h"
-#include "GameScripts.h"
 #include "Input.h"
 #include "LevelManager.h"
 #include "TimeManager.h"
 class EngineManager
 {
 };
-

16ef940ab4ebab659777c25bc63d10ef6a724bc2
wdamour <wdamour@gaming.tech>
2024-03-12 12:52:41 +0100

    SplashScreen

--- Game/SplashScreen.cpp
+++ Game/SplashScreen.cpp
@@ -3,6 +3,7 @@
 SplashScreen::SplashScreen(std::wstring filename, int nDuration) {
     image = new Image(filename);
     //m_StartTime = DXUtil.Timer(TIMER.GETABSOLUTETIME);
+    QueryPerformanceCounter((LARGE_INTEGER*)&m_StartTime);
     m_EndTime = m_StartTime + nDuration;
     //m_ vb = new VertexBuffer(typeof(CustomVertex.TransformedTextured), 4, CGameEngine.Device3D, Usage.WriteOnly, CustomVertex.TransformedTextured.Format, Pool.Default);
 }
--- Game/SplashScreen.h
+++ Game/SplashScreen.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "Image.h"
+#include <Windows.h>
 class SplashScreen 
 {
 private:
     Image* image;
-    float m_StartTime;
+    __int64 m_StartTime;
+    //float m_StartTime;
     float m_EndTime;
+
     //private VertexBuffer m_vb;
+    ID3D11Buffer* m_vb;
 public:
     bool m_bInitialized = false;
     float fTimeLeft;

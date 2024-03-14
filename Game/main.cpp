349095579a3112850beea9fda6c4cc4827a81f36
wdamour <wdamour@gaming.tech>
2024-03-14 14:40:33 +0100

    component for image?

--- Game/main.cpp
+++ Game/main.cpp
@@ -2,7 +2,12 @@
 #include "Window.h"
 #include "SplashScreen.h"
 #include "Image.h"
-
+//-----------------------
+/*
+#include "../Engine/ComponentRenderMesh.h"
+#include "../Engine/EngineObject.h"
+#include "../Engine/GeometryHandler.h"
+*/
 int WinMain() {
 	ComPtr<ID3D12Device> device;
 	D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device));
@@ -27,5 +32,18 @@ int WinMain() {
 
 	commandList->SetDescriptorHeaps(1, srvHeap.GetAddressOf());
 	commandList->SetGraphicsRootDescriptorTable(0, srvHeap->GetGPUDescriptorHandleForHeapStart()); 
+
+
+	//----------------------------- 
+	//A tester et modifier
+	/*
+	EngineObject oEngineOject = EngineObject(1,1,1);
+	Texture newTexture;
+	newTexture.Resource = texture;
+	Shader* shader;
+	GeometryHandler geoHan;
+	GeometryHandler::Mesh mesh = geoHan.BuildBox(1,1,1,1);
+	ComponentRenderMesh splashRender = ComponentRenderMesh(&oEngineOject, mesh, shader, &newTexture);
+	*/
 	return 0;
 }
